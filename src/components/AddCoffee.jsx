4ad6721672ca7b552611e8fb1241d65e5f65b5c2
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-12 17:32:55 +0600

    [ADD]-send-coffee-data-to-the-server-and-store-in-the-mongodb-database

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 const AddCoffee = () => {
   const handleAddCoffee = (e) => {
     e.preventDefault();
@@ -22,6 +23,25 @@ const AddCoffee = () => {
     };
 
     console.log(newCoffee);
+
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        Swal.fire({
+          title: "Success!",
+          text: "New coffee added successfully",
+          icon: "success",
+          confirmButtonText: "Cool",
+        });
+      });
+    form.reset();
   };
 
   return (
