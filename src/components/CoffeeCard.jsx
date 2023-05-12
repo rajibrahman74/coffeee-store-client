5a44fc5f877ed8fe7af058ddd43ecc930b9330fd
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-12 19:53:01 +0600

    [ADD]-delete-a-coffee-and-fetch-coffee-for-udpate

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,5 +1,37 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="card card-side shadow-xl bg-[#F5F4F1] flex items-center justify-center p-3">
       <figure>
@@ -12,12 +44,30 @@ const CoffeeCard = ({ coffee }) => {
         <p>Taste: {taste}</p>
       </div>
       <div className="btn-group btn-group-vertical space-y-4">
-        <button className="btn bg-[#ab8f6a] border-0 hover:bg-[#827360e4]">View</button>
-        <button className="btn">Edit</button>
-        <button className="btn bg-red-600 hover:bg-red-700 border-0">Delete</button>
+        <button className="btn bg-[#ab8f6a] border-0 hover:bg-[#827360e4]">
+          View
+        </button>
+        <Link to={`updatecoffee/:${_id}`}></Link>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn bg-red-600 hover:bg-red-700 border-0"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
+};
+
+CoffeeCard.propTypes = {
+  coffee: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    supplier: PropTypes.string.isRequired,
+    taste: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default CoffeeCard;
